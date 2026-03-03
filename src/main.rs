6ba051df3e8c99651dc0b6bbfb1b12b24a7c1ee4
Edwin Ekeinde <eekeind2@students.towson.edu>
2026-03-02 23:10:13 -0500

    Removed target folder from tracking and updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    let test1 = "ABC123DEF".to_string();
+    let test2 = "123ABC".to_string();
+
+    assert_eq!(q1_parser(test1), true);
+    assert_eq!(q1_parser(test2), false);
+}
+
+fn is_uppercase_or_digit(c: char) -> bool {
+    is_uppercase_letter(c) || (c >= '0' && c <= '9')
+}
+
+fn is_uppercase_letter(c: char) -> bool {
+    c >= 'A' && c <= 'Z'
+}
+
+fn q1_parser(text: String) -> bool {
+    let characters_array: Vec<char> = text.chars().collect();
+
+    if characters_array.len() < 2 {
+        return false;
+    }
+
+    for (i, character) in characters_array.iter().enumerate() {
+
+        if i == 0 || i == 1 {
+            if !is_uppercase_letter(*character) {
+                return false;
+            }
+        } else {
+            if !is_uppercase_or_digit(*character) {
+                return false;
+            }
+        }
+    }
+
+    true
 }
